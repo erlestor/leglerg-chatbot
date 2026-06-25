f7c8e0473b0f5bf63d8437807466ee3249b7da09
Erlend Storsve <erlendstorsve@gmail.com>
2026-06-25 23:40:46 +0300

    better randomization using deck shuffling

--- readme.md
+++ readme.md
@@ -2,9 +2,11 @@
 
 ## Todo
 
-- [ ] få den te å responder med random bilda istedenfor tekst. mangle å ha den så den e random, men ikke gjentar sæ
-- [ ] gjør den rasker te å svar. komprimer bildan te helvete tenke æ
+- [x] få den te å responder med random bilda istedenfor tekst. mangle å ha den så den e random, men ikke gjentar sæ
+- [ ] få den te å gjør det med alle meldingan æ skriv
 - [ ] hoste den en plass der den kan kjør permanent, egen server eller hoste-side som e stabil og ikke endre prising med det samme
+- [ ] endre den te å reager på steinar sine meldinga
+- [ ] gjør den rasker te å svar. teste på pc hjem først
 
 ## Problems
 
@@ -14,3 +16,7 @@
 
 - hver gang steinar sende nokka. blir fort plagsomt though. men æ kan endre det etter steinar har klikka nokk
 - mange keywords for f1. forrige gang f1 ble nevnt i discord derimot så va det bare et bilde og æ ble tagga
+
+### Brainstorming
+
+- Okei. Æ tenke den her e jo veldig retta mot steinar. Så den med hver gang steinar skriv nokka virke best. Kan skru den av hvis det blir plagsomt. Men da får æ et ragebait øyeblikk som gir payoff. Så kan endres te bare om f1 seiner. Chatten e lite brukt som e plagsomt, men jaja. Får nå det ene øyeblikket innen en måneds tid ellernåsånt. Kan jo spør han om å send nokka i general chat. Typ hvis han har et artig bilde å vis
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,54 @@
-import { Client } from "discord.js";
-import { config } from "./config";
-import { commands } from "./commands";
-import { deployCommands } from "./deploy-commands";
+import fs from "fs"
+import path from "path"
+import { AttachmentBuilder, Client } from "discord.js"
+
+import { deployCommands } from "./deploy-commands"
+import { config } from "./config"
+import { commands } from "./commands"
+import { nextIndex } from "./utils/random"
 
 const client = new Client({
   intents: ["Guilds", "GuildMessages", "DirectMessages", "MessageContent"],
-});
+})
 client.once("ready", () => {
-  console.log("Discord bot is ready! 🤖");
-});
+  console.log("Discord bot is ready! 🤖")
+})
 
 // register commands when added to a server
 client.on("guildCreate", async (guild) => {
-  await deployCommands({ guildId: guild.id });
-});
+  await deployCommands({ guildId: guild.id })
+})
 
 // article says "Run corresponding command when new user interaction has been created"
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) {
-    return;
+    return
   }
-  const { commandName } = interaction;
+  const { commandName } = interaction
   if (commands[commandName as keyof typeof commands]) {
-    commands[commandName as keyof typeof commands].execute(interaction);
+    commands[commandName as keyof typeof commands].execute(interaction)
   }
-});
+})
 
+const imagePaths = fs
+  .readdirSync("./images/compressed")
+  .filter((file) => file.endsWith(".jpg"))
+  .map((file) => path.join("./images/compressed", file))
+
+const images = imagePaths.map((imagePath) => new AttachmentBuilder(imagePath))
+
+// send leclerc images
 client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
+  if (message.author.bot) return
 
   // 11 is the amount of images, make dynamic based on directory
-  const randomIndex = Math.floor(Math.random() * 11) + 1;
-  const imageName = randomIndex.toString() + ".jpg";
+  const randomImage = images[nextIndex()]
 
   if (message.content.toLowerCase().includes("f1")) {
-    // message.channel.send("leclerc washed");
     message.channel.send({
-      files: ["./images/compressed/" + imageName],
-    });
+      files: [randomImage],
+    })
   }
-});
+})
 
-client.login(config.DISCORD_TOKEN);
+client.login(config.DISCORD_TOKEN)
